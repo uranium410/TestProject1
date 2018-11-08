21ecc22841d92f2333f80aed044b8910a4713a40
uranium410 <36751459+uranium410@users.noreply.github.com>
2018-11-09 01:14:15 +0900

    20181108

--- TestProject1/InputManager.cpp
+++ TestProject1/InputManager.cpp
@@ -8,17 +8,21 @@
 //↑からお借りした関数です。
 //ref https://msdn.microsoft.com/ja-jp/library/cc351699.aspx
 //デバイス列挙用のコールバック関数
-BOOL WINAPI DIEnumDevCallback(
+BOOL CALLBACK DIEnumDevBySemanticsCallback(
 	LPCDIDEVICEINSTANCE lpddi,
+	LPDIRECTINPUTDEVICE8 lpdid,
+	DWORD dwFlags,
+	DWORD dwRemaining,
 	LPVOID pvRef
 )
 {
 	// 第2引数がDIDEVICEINSTANCE構造体のvecor STLだとします
-	std::vector<DIDEVICEINSTANCE>* ptr = (std::vector<DIDEVICEINSTANCE>*)pvRef;
-	DIDEVICEINSTANCE tmp = *lpddi;
+	//std::vector<DIDEVICEINSTANCE>* ptr = (std::vector<DIDEVICEINSTANCE>*)pvRef;
+	//DIDEVICEINSTANCE tmp = *lpdi;
+	std::vector<LPCDIDEVICEINSTANCE>* ptr = (std::vector<LPCDIDEVICEINSTANCE>*)pvRef;
 
 	// 配列に格納
-	ptr->push_back(tmp);
+	if(dwFlags == DIEDBS_MAPPEDPRI1)ptr->push_back(lpddi);
 
 	// 列挙を続ける場合はDIENUM_CONTINUE、止める場合はDIENUM_STOPを指定
 	// 全ての列挙が終了すると自動的にコールバックも終了するので、
@@ -30,6 +34,22 @@ BOOL WINAPI DIEnumDevCallback(
 namespace BasicSystem {
 
 	InputManager::InputManager(HINSTANCE _hInst, HWND _hWnd) {
+		
+		
+
+		//DIACTIONFORMATの定義
+		p1ActionFormat.dwSize		= sizeof(DIACTIONFORMAT);
+		p1ActionFormat.dwActionSize = sizeof(DIACTION);
+		p1ActionFormat.dwDataSize	= NUMBER_OF_ACTIONS * sizeof(DWORD);
+		p1ActionFormat.dwNumActions = NUMBER_OF_ACTIONS;
+//		p1ActionFormat.guidActionMap = p1Action->guidInstance;
+		p1ActionFormat.dwGenre		= DIVIRTUAL_FIGHTING_THIRDPERSON;
+		p1ActionFormat.rgoAction	= p1Action;
+		p1ActionFormat.dwBufferSize = 16;
+		p1ActionFormat.lAxisMin		= -100;
+		p1ActionFormat.lAxisMax		= 100;
+		wcscpy_s(p1ActionFormat.tszActionMap, L"DI Test");
+
 		inputData = std::make_shared<InputData>();
 		//ウィンドウハンドル取得
 		hWnd = _hWnd;
@@ -52,18 +72,45 @@ namespace BasicSystem {
 
 		//ゲームパッドデバイス取得//
 		//ゲームパッドデバイス列挙
+		/*
 		lpDirect8->EnumDevices(DI8DEVCLASS_GAMECTRL, //全てのデバイス列挙
 			DIEnumDevCallback,
 			&gamePads,
-			DIEDFL_ATTACHEDONLY);
+			DIEDFL_ATTACHEDONLY);*/
+		hr = lpDirect8->EnumDevicesBySemantics(
+			NULL,
+			&p1ActionFormat,
+			DIEnumDevBySemanticsCallback,
+			&gamePads,
+			DIEDBSFL_ATTACHEDONLY
+		);
 
 		gamePadDeviceNum = gamePads.size();
 
-		if (gamePadDeviceNum >= 1) {
-			hr = lpDirect8->CreateDevice(gamePads[0].guidInstance, &GamePad1, NULL);
-		}
-		if (gamePadDeviceNum >= 2) {
-			hr = lpDirect8->CreateDevice(gamePads[1].guidInstance, &GamePad1, NULL);
+		if (gamePadDeviceNum > 0) {
+			lpDirect8->CreateDevice(gamePads[0]->guidInstance, &GamePad1, NULL);
+			
+			GamePad1->BuildActionMap(&p1ActionFormat, NULL, 0L);
+			GamePad1->SetActionMap(&p1ActionFormat, NULL, 0L);
+
+			//アクションマップの表示
+			DICOLORSET dics;
+			ZeroMemory(&dics, sizeof(DICOLORSET));
+			dics.dwSize = sizeof(DICOLORSET);
+
+			DICONFIGUREDEVICESPARAMS dicdp;
+			ZeroMemory(&dicdp, sizeof(dicdp));
+			dicdp.dwSize = sizeof(dicdp);
+			dicdp.dwcUsers = 1;
+			dicdp.lptszUserNames = NULL;
+
+			dicdp.dwcFormats = 1;
+			dicdp.lprgFormats = &p1ActionFormat;
+			dicdp.hwnd = _hWnd;
+			dicdp.lpUnkDDSTarget = NULL;
+
+			lpDirect8->ConfigureDevices(NULL, &dicdp, DICD_EDIT, NULL);
+
 		}
 	}
 
@@ -82,16 +129,38 @@ namespace BasicSystem {
 
 		if (KEYDOWN(keyBuffer, DIK_ESCAPE))keyDownESC = true;
 		else keyDownESC = false;
-
+		
 		inputData->P1_Up.SetFlag(KEYDOWN(keyBuffer, DIK_W));
 		inputData->P1_Down.SetFlag(KEYDOWN(keyBuffer, DIK_S));
 		inputData->P1_Right.SetFlag(KEYDOWN(keyBuffer, DIK_D));
 		inputData->P1_Left.SetFlag(KEYDOWN(keyBuffer, DIK_A));
 		inputData->P1_J.SetFlag(KEYDOWN(keyBuffer,DIK_J));
 		inputData->P1_K.SetFlag(KEYDOWN(keyBuffer,DIK_K));
+		
+		return;
 
 		//ゲームパッド
 		if (gamePadDeviceNum >= 1) {
+			//アクセス権の取得
+			GamePad1->Poll();
+			hr = GamePad1->Acquire();
+
+			DIDEVICEOBJECTDATA getData;
+			GamePad1->GetDeviceData(sizeof(DIDEVICEOBJECTDATA),&getData,NULL,0);
+
+			//情報取得
+			DIJOYSTATE dijs;
+			ZeroMemory(&dijs, sizeof(DIJOYSTATE));
+			GamePad1->GetDeviceState(sizeof(DIJOYSTATE), &dijs);
+
+			inputData->P1_Up.SetFlag(dijs.rgbButtons[0]);
+			inputData->P1_Down.SetFlag(dijs.rgbButtons[1]);
+			inputData->P1_Right.SetFlag(dijs.rgbButtons[2]);
+			inputData->P1_Left.SetFlag(dijs.rgbButtons[3]);
+			inputData->P1_J.SetFlag(dijs.rgbButtons[4]);
+			inputData->P1_K.SetFlag(dijs.rgbButtons[5]);
+		}
+		if (gamePadDeviceNum >= 2) {
 			//アクセス権の取得
 			GamePad1->Poll();
 			hr = GamePad1->Acquire();
@@ -100,6 +169,7 @@ namespace BasicSystem {
 			DIJOYSTATE dijs;
 			ZeroMemory(&dijs, sizeof(DIJOYSTATE));
 			GamePad1->GetDeviceState(sizeof(DIJOYSTATE), &dijs);
+
 		}
 		return;
 	}
--- TestProject1/InputManager.h
+++ TestProject1/InputManager.h
@@ -1,6 +1,36 @@
 #pragma once
 #include "AllHead.h"
 
+//アクションマップの定義
+//アクション定義
+enum eGameActions
+{
+	P1_A_LATERAL,
+	P1_A_VIRTICAL,
+	P1_B_UP,
+	P1_B_DOWN,
+	P1_B_RIGHT,
+	P1_B_LEFT,
+	P1_B1,
+	P1_B2,
+	P1_START
+};
+
+/*
+三人称型(DIVIRITUAL_FIGHTING_THIRDPERSON)のアクションマッピング定数を想定
+優先順位1のコントロール:
+DIAXIS_TPS_MOVE
+DIAXIS_TPS_TURN
+DIBUTTON_TPS_ACTION
+DIBUTTON_TPS_JUMP
+DIBUTTON_TPS_MENU
+DIBUTTON_TPS_RUN
+DIBUTTON_TPS_SELECT
+DIBUTTON_TPS_USE
+*/
+
+#define NUMBER_OF_ACTIONS 5
+
 namespace BasicSystem {
 
 	class InputFlag {
@@ -44,6 +74,27 @@ namespace BasicSystem {
 	};
 
 	class InputManager {
+		//アクションマップの定義
+		DIACTION p1Action[NUMBER_OF_ACTIONS] =
+		{
+			{P1_A_LATERAL,	DIAXIS_TPS_MOVE,	0,	L"LATERAL",	},
+			{P1_A_VIRTICAL,	DIAXIS_TPS_TURN,	0,	L"VIRTICAL",},
+			{P1_B1,			DIBUTTON_TPS_JUMP,	0,	L"B1",		},
+			{P1_B2,			DIBUTTON_TPS_RUN,	0,	L"B2",		},
+			{P1_START,		DIBUTTON_TPS_MENU,	0,	L"START"}
+		};
+
+		DIACTION p2Action[NUMBER_OF_ACTIONS] =
+		{
+			{P1_A_LATERAL,	DIAXIS_TPS_MOVE,	0,	L"LATERAL",	},
+			{P1_A_VIRTICAL,	DIAXIS_TPS_TURN,	0,	L"VIRTICAL",},
+			{P1_B1,			DIBUTTON_TPS_JUMP,	0,	L"B1",		},
+			{P1_B2,			DIBUTTON_TPS_RUN,	0,	L"B2",		},
+			{P1_START,		DIBUTTON_TPS_MENU,	0,	L"START"}
+		};
+		//アクションフォーマット
+		DIACTIONFORMAT p1ActionFormat;
+
 		//キーボードの入力データ格納用
 		char keyBuffer[256];
 		//ウィンドウハンドル
@@ -54,7 +105,7 @@ namespace BasicSystem {
 		//DirectInput8デバイスアドレス
 		LPDIRECTINPUT8 lpDirect8;
 		//列挙したゲームパッドデバイスを格納する
-		std::vector<DIDEVICEINSTANCE> gamePads;
+		std::vector<LPCDIDEVICEINSTANCE> gamePads;
 		//キーボードデバイス
 		LPDIRECTINPUTDEVICE8 lpdiKeyboard;
 		//ゲームパッド
